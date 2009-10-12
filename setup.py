8560ce7e68c634b7c090c145933434f2cad3d741
Hugo Doria <hugo@archlinux.org>
2009-10-12 18:42:31 -0300

    Updating NEWS, TODO and setup.py (2.4 release)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ DATAFILES = [('/usr/share/man/man1', ['namcap.1']),
 		('/usr/share/doc/namcap',['README','AUTHORS','TODO'])]
 
 setup(name="namcap",
-	version="2.3",
+	version="2.4",
 	description="Pacman package analyzer",
 	author="Jason Chu",
 	author_email="jason@archlinux.org",
