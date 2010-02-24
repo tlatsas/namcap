278d7582ef722c42af263a80a94c6948d1a619b3
Dan McGee <dan@archlinux.org>
2010-02-23 22:08:02 -0600

    Allow static libraries to have 644 or 444 permission
    Addresses FS#15749.
    
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- Namcap/permissions.py
+++ Namcap/permissions.py
@@ -35,7 +35,7 @@ class package:
 				ret[1].append(("file-world-writable %s", i.name))
 			if not i.mode & 1 and i.isdir():
 				ret[1].append(("directory-not-world-executable %s", i.name))
-			if str(i.name).endswith('.a') and i.mode != 0644:
+			if str(i.name).endswith('.a') and i.mode != 0644 and i.mode != 0444:
 				ret[1].append(("incorrect-library-permissions %s", i.name))
 		return ret
 	def type(self):
