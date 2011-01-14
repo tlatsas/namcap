ed51f5622ba6837a49e1f9fc46baa3cd0d771b82
Dan McGee <dan@archlinux.org>
2011-01-14 00:56:14 -0600

    Add rule to detect missing backup files
    Signed-off-by: Dan McGee <dan@archlinux.org>

--- Namcap/missingbackups.py
+++ Namcap/missingbackups.py
@@ -1,6 +1,6 @@
 # 
-# namcap rules - __init__
-# Copyright (C) 2003-2009 Jason Chu <jason@archlinux.org>
+# namcap rules - missingbackups
+# Copyright (C) 2011 Dan McGee <dan@archlinux.org>
 # 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -17,48 +17,23 @@
 #   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # 
 
-__tarball__ = """
-  anyelf
-  capsnamespkg
-  depends
-  directoryname
-  elffiles
-  emptydir
-  fhsmanpages
-  fhsinfopages
-  fileownership
-  gnomemime
-  hicoloricons
-  infodirectory
-  libtool
-  licensepkg
-  lotsofdocs
-  mimefiles
-  missingbackups
-  perllocal
-  permissions
-  scrollkeeper
-  symlink
-  urlpkg
-
-""".split()
-
-__pkgbuild__ = """
-  arrays
-  badbackups
-  capsnames
-  carch
-  checksums
-  invalidstartdir
-  license
-  pkgname
-  rpath
-  sfurl
-  tags
-  url
-
-""".split()
-
-__all__ = __tarball__ + __pkgbuild__
-
+class package:
+	def short_name(self):
+		return "missingbackups"
+	def long_name(self):
+		return "Backup files listed in package should exist"
+	def prereq(self):
+		return "tar"
+	def analyze(self, pkginfo, tar):
+		ret = [[], [], []]
+		if not hasattr(pkginfo, 'backup') or len(pkginfo.backup) == 0:
+			return ret
+		known_backups = set(pkginfo.backup)
+		found_backups = [x for x in tar.getnames() if x in known_backups]
+		missing_backups = known_backups - set(found_backups)
+		for backup in missing_backups:
+			ret[0].append(("missing-backup-file %s", backup))
+		return ret
+	def type(self):
+		return "tarball"
 # vim: set ts=4 sw=4 noet:
