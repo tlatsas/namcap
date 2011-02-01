dcab84d6fb662084f6423ab7e94616a982883ceb
Rémy Oudompheng <remy@archlinux.org>
2011-02-01 01:09:22 +0100

    Update tests with new rule location.

--- Namcap/tests/pkgbuild/test_pkginfo.py
+++ Namcap/tests/pkgbuild/test_pkginfo.py
@@ -6,101 +6,19 @@ import shutil
 import pacman
 import valid_pkgbuilds
 
-from Namcap.rules.missingvars import *
+from Namcap.rules.pkginfo import *
 
 EMPTY_RESULT = [ [] , [] , [] ]
 
-class NamcapChecksumTest(unittest.TestCase):
+class NamcapInvalidPkgNameTest(unittest.TestCase):
 	pkgbuild1 = """
 # Maintainer: Arch Linux <archlinux@example.com>
 # Contributor: Arch Linux <archlinux@example.com>
 
-pkgname=mypackage
+pkgname=MyPackage
 pkgver=1.0
 pkgrel=1
 pkgdesc="A package"
-arch=('i686' 'x86_64')
-url="http://www.example.com/"
-license=('GPL')
-depends=('glibc')
-options=('!libtool')
-source=(ftp://ftp.example.com/pub/mypackage-0.1.tar.gz)
-md5sums=('look-this-is-an-invalid-checksum')
-
-build() {
-  true
-}
-
-package() {
-  true
-}
-"""
-	pkgbuild2 = """
-# Maintainer: Arch Linux <archlinux@example.com>
-# Contributor: Arch Linux <archlinux@example.com>
-
-pkgname=mypackage
-pkgver=1.0
-pkgrel=1
-pkgdesc="A package"
-arch=('i686' 'x86_64')
-url="http://www.example.com/"
-license=('GPL')
-depends=('glibc')
-options=('!libtool')
-source=(ftp://ftp.example.com/pub/mypackage-0.1.tar.gz
-		nice-patch.diff)
-md5sums=('abcdefabcdef12345678901234567890')
-
-build() {
-  true
-}
-
-package() {
-  true
-}
-"""
-	def run_on_pkg(self, p):
-		with open(self.tmpname, 'w') as f:
-			f.write(p)
-		pkginfo = pacman.load(self.tmpname)
-		return self.rule.analyze(pkginfo, self.tmpname)
-
-	def runTest(self):
-		self.rule = ChecksumsRule()
-		self.tmpdir = tempfile.mkdtemp()
-		self.tmpname = os.path.join(self.tmpdir, "PKGBUILD")
-
-		# Valid PKGBUILDS
-		for p in valid_pkgbuilds.all_pkgbuilds:
-			ret = self.run_on_pkg(p)
-			self.assertEqual(ret, EMPTY_RESULT)
-
-		# Example 1
-		ret = self.run_on_pkg(self.pkgbuild1)
-		self.assertEqual(ret[0], [
-			("improper-checksum %s %s", ("md5sums",
-			 "look-this-is-an-invalid-checksum"))
-		])
-		self.assertEqual(ret[1], [])
-		self.assertEqual(ret[2], [])
-
-		# Example 2
-		ret = self.run_on_pkg(self.pkgbuild2)
-		self.assertEqual(ret[0], [
-			("not-enough-checksums %s %i needed", ("md5sums", 2))
-		])
-		self.assertEqual(ret[1], [])
-		self.assertEqual(ret[2], [])
-
-		shutil.rmtree(self.tmpdir)
-
-class NamcapMaintainerTagTest(unittest.TestCase):
-	pkgbuild1 = """
-pkgname=mypackage
-pkgver=1.0
-pkgrel=1
-pkgdesc="The foobar program"
 arch=('i686' 'x86_64')
 url="http://www.example.com/"
 license=('GPL')
@@ -128,7 +46,7 @@ package() {
 		return self.rule.analyze(pkginfo, self.tmpname)
 
 	def runTest(self):
-		self.rule = TagsRule()
+		self.rule = CapsPkgnameRule()
 		self.tmpdir = tempfile.mkdtemp()
 		self.tmpname = os.path.join(self.tmpdir, "PKGBUILD")
 
@@ -139,9 +57,113 @@ package() {
 
 		# Example 1
 		ret = self.run_on_pkg(self.pkgbuild1)
-		self.assertEqual(ret[0], [])
-		self.assertEqual(ret[1], [("missing-maintainer", ())])
-		self.assertEqual(ret[2], [("missing-contributor", ())] )
+		self.assertEqual(ret[0], [("package-name-in-uppercase", ())])
+		self.assertEqual(ret[1], [])
+		self.assertEqual(ret[2], [])
+
+		shutil.rmtree(self.tmpdir)
+
+class NamcapLicenseTest(unittest.TestCase):
+	pkgbuild1 = """
+# Maintainer: Arch Linux <archlinux@example.com>
+# Contributor: Arch Linux <archlinux@example.com>
+
+pkgname=mypackage
+pkgver=1.0
+pkgrel=1
+pkgdesc="A package"
+arch=('i686' 'x86_64')
+url="http://www.example.com/"
+depends=('glibc')
+options=('!libtool')
+source=(ftp://ftp.example.com/pub/mypackage-0.1.tar.gz)
+md5sums=('abcdefabcdef12345678901234567890')
+
+build() {
+  cd "${srcdir}"/${pkgname}-${pkgver}
+  ./configure --prefix=/usr
+  make
+}
+
+package() {
+  cd "${srcdir}"/${pkgname}-${pkgver}
+  ./configure --prefix=/usr
+  make DESTDIR="${pkgdir}" install
+}
+"""
+	def run_on_pkg(self, p):
+		with open(self.tmpname, 'w') as f:
+			f.write(p)
+		pkginfo = pacman.load(self.tmpname)
+		return self.rule.analyze(pkginfo, self.tmpname)
+
+	def runTest(self):
+		self.rule = LicenseRule()
+		self.tmpdir = tempfile.mkdtemp()
+		self.tmpname = os.path.join(self.tmpdir, "PKGBUILD")
+
+		# Valid PKGBUILDS
+		for p in valid_pkgbuilds.all_pkgbuilds:
+			ret = self.run_on_pkg(p)
+			self.assertEqual(ret, EMPTY_RESULT)
+
+		# Example 1
+		ret = self.run_on_pkg(self.pkgbuild1)
+		self.assertEqual(ret[0], [("missing-license", ())] )
+		self.assertEqual(ret[1], [])
+		self.assertEqual(ret[2], [])
+
+		shutil.rmtree(self.tmpdir)
+
+class NamcapUrlTest(unittest.TestCase):
+	pkgbuild1 = """
+# Maintainer: Arch Linux <archlinux@example.com>
+# Contributor: Arch Linux <archlinux@example.com>
+
+pkgname=mypackage
+pkgver=1.0
+pkgrel=1
+pkgdesc="A package"
+arch=('i686' 'x86_64')
+depends=('glibc')
+license=('GPL')
+options=('!libtool')
+source=(ftp://ftp.example.com/pub/mypackage-0.1.tar.gz)
+md5sums=('abcdefabcdef12345678901234567890')
+
+build() {
+  cd "${srcdir}"/${pkgname}-${pkgver}
+  ./configure --prefix=/usr
+  make
+}
+
+package() {
+  cd "${srcdir}"/${pkgname}-${pkgver}
+  ./configure --prefix=/usr
+  make DESTDIR="${pkgdir}" install
+}
+"""
+	def run_on_pkg(self, p):
+		with open(self.tmpname, 'w') as f:
+			f.write(p)
+		pkginfo = pacman.load(self.tmpname)
+		return self.rule.analyze(pkginfo, self.tmpname)
+
+	def runTest(self):
+		self.rule = UrlRule()
+		self.tmpdir = tempfile.mkdtemp()
+		self.tmpname = os.path.join(self.tmpdir, "PKGBUILD")
+
+		# Valid PKGBUILDS
+		for p in valid_pkgbuilds.all_pkgbuilds:
+			ret = self.run_on_pkg(p)
+			self.assertEqual(ret, EMPTY_RESULT)
+
+		# Example 1
+		ret = self.run_on_pkg(self.pkgbuild1)
+		self.assertEqual(ret[0], [("missing-url", ())] )
+		self.assertEqual(ret[1], [])
+		self.assertEqual(ret[2], [])
 
 		shutil.rmtree(self.tmpdir)
 
