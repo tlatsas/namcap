7e6118d77794ecc8c556cf4828c10589444536f6
Rémy Oudompheng <remy@archlinux.org>
2011-02-17 00:11:31 +0100

    Rework rpath rule and add a unit test for it.
    The rpath rule is no longer a rule applying to extracted tarballs.
    This makes it easier to include it within our test frameork. However
    since readelf does not want to read from stdin, ELF files are temporarily
    extracted.

--- Namcap/rules/rpath.py
+++ Namcap/rules/rpath.py
@@ -18,6 +18,7 @@
 #
 
 import os, subprocess, re
+import tempfile
 from Namcap.util import is_elf, clean_filename
 from Namcap.ruleclass import *
 
@@ -26,49 +27,65 @@ allowed_toplevels = [s + '/' for s in allowed]
 warn = ['/usr/local/lib']
 libpath = re.compile('Library rpath: \[(.*)\]')
 
-def checkrpath(filename, ret):
-	"Checks if secure RPATH."
-
-	if not is_elf(filename):
-		return
-
-	var = subprocess.Popen('readelf -d ' + filename,
-			shell=True, env={'LANG': 'C'},
-			stdout=subprocess.PIPE,
-			stderr=subprocess.PIPE).communicate()
+def get_rpaths(filename):
+	p = subprocess.Popen(["readelf", "-d", filename],
+		env={'LANG': 'C'},
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE)
+	var = p.communicate()
+	if p.returncode != 0:
+		raise IOError("unable to read ELF file")
 
 	for j in var[0].decode('ascii').splitlines():
 		n = libpath.search(j)
 		# Is this a Library rpath: line?
-		if n != None:
-			if ":" in n.group(1):
-				rpaths = n.group(1).split(':')
-			else:
-				rpaths = [n.group(1)]
-			for path in rpaths:
-				path_ok = path in allowed
-				for allowed_toplevel in allowed_toplevels:
-					if path.startswith(allowed_toplevel):
-						path_ok = True
-				fname = clean_filename(filename)
-				if not path_ok:
-					ret[0].append(("insecure-rpath %s %s", (path, fname)))
-					break
-				if path in warn and fname not in insecure_rpaths:
-					ret[1].append(("insecure-rpath %s %s", (path, fname)))
+		if n is None:
+			continue
+
+		if ":" in n.group(1):
+			rpaths = n.group(1).split(':')
+		else:
+			rpaths = [n.group(1)]
+		for path in rpaths:
+			yield path
 
 class package(TarballRule):
 	name = "rpath"
 	description = "Verifies correct and secure RPATH for files."
-	def prereq(self):
-		return "extract"
-	def analyze(self, pkginfo, data):
+	def analyze(self, pkginfo, tar):
 		ret = [[], [], []]
-		insecure_rpaths = [[], []]
 
-		for dirpath, subdirs, files in os.walk(data):
-			for i in files:
-				checkrpath(os.path.join(dirpath, i), ret)
+		for entry in tar:
+			if not entry.isfile():
+				continue
+
+			# is it an ELF file ?
+			f = tar.extractfile(entry)
+			elf = f.read()
+			f.close()
+			if elf[:4] != b"\x7fELF":
+				continue # not an ELF file
+
+			# write it to a temporary file
+			f = tempfile.NamedTemporaryFile(delete = False)
+			f.write(elf)
+			f.close()
+
+			for path in get_rpaths(f.name):
+				path_ok = path in allowed
+				for allowed_toplevel in allowed_toplevels:
+					if path.startswith(allowed_toplevel):
+						path_ok = True
+
+				if not path_ok:
+					ret[0].append(("insecure-rpath %s %s",
+						(path, entry.name)))
+					break
+				if path in warn and entry.name not in insecure_rpaths:
+					ret[1].append(("insecure-rpath %s %s",
+						(path, entry.name)))
+
+			os.unlink(f.name)
 
 		return ret
 
---
+++ Namcap/tests/package/test_rpath.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+#
+# namcap tests - rpath
+# Copyright (C) 2011 Rémy Oudompheng <remy@archlinux.org>
+# 
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+#   USA
+# 
+
+import os
+from Namcap.tests.makepkg import MakepkgTest
+import Namcap.rules.rpath
+
+class RpathTest(MakepkgTest):
+	pkgbuild = """
+pkgname=__namcap_test_rpath
+pkgver=1.0
+pkgrel=1
+pkgdesc="A package"
+arch=('i686' 'x86_64')
+url="http://www.example.com/"
+license=('GPL')
+depends=('glibc')
+source=()
+options=(!purge !zipman)
+build() {
+  cd "${srcdir}"
+  echo "int main() { return 0; }" > main.c
+  /usr/bin/gcc -o main.o -c main.c
+  /usr/bin/ld -o main --rpath /home/evil/lib main.o -lc
+}
+package() {
+  install -D -m 755 "$srcdir/main" "$pkgdir/usr/bin/evilprogram"
+}
+"""
+	def test_rpath_files(self):
+		pkgfile = "__namcap_test_rpath-1.0-1-%(arch)s.pkg.tar" % { "arch": self.arch }
+		with open(os.path.join(self.tmpdir, "PKGBUILD"), "w") as f:
+			f.write(self.pkgbuild)
+		self.run_makepkg()
+		ret = self.run_rule_on_tarball(
+				os.path.join(self.tmpdir, pkgfile),
+				Namcap.rules.rpath.package
+				)
+		self.assertEqual(ret[0], [
+			("insecure-rpath %s %s",
+			("/home/evil/lib", "usr/bin/evilprogram"))
+		])
+		self.assertEqual(ret[1], [])
+		self.assertEqual(ret[2], [])
+
+# vim: set ts=4 sw=4 noet:
+
