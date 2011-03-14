e2044d4583f80cd6283a2f8d0ffa1d6f5d834123
Rémy Oudompheng <remy@archlinux.org>
2011-03-14 23:14:52 +0100

    Reduce the amount of false positives in extravars rule.
    A typical case is an array of unquoted, versioned dependencies
    which spans over several lines. False positives may still appear
    in the case of multiline string literals containing the '=' sign.

--- Namcap/rules/extravars.py
+++ Namcap/rules/extravars.py
@@ -18,7 +18,46 @@
 # 
 
 import re
+import os
+import subprocess
+import tempfile
 from Namcap.ruleclass import *
+
+BASH_BUILTINS = set([
+	"BASH", "BASHOPTS", "BASH_ALIASES", "BASH_ARGC", "BASH_ARGV", "BASH_CMDS", "BASH_LINENO",
+	"BASH_SOURCE", "BASH_VERSINFO", "BASH_VERSION", "DIRSTACK", "EUID", "GROUPS", "HOSTNAME",
+	"HOSTTYPE", "IFS", "MACHTYPE", "OPTERR", "OPTIND", "OSTYPE", "PATH", "PIPESTATUS", "PPID",
+	"PS1", "PS2", "PS3", "PS4",
+	"PWD", "SHELL", "SHELLOPTS", "SHLVL", "TERM", "UID",
+	])
+
+def find_lowercase_global_vars(pkgbuild):
+	# list the variable names beginning with a lowercase letter
+	varnames = []
+
+	# write an annotated PKGBUILD to a temporary file
+	f = tempfile.NamedTemporaryFile(delete = False)
+	annotated_pkgbuild = pkgbuild + "\ndeclare +x"
+	f.write(annotated_pkgbuild.encode('utf-8'))
+	f.close()
+	# run bash -r on it to list variables
+	cwd = os.getcwd()
+	os.chdir(os.path.dirname(f.name))
+	p = subprocess.Popen(["bash", "-r", os.path.basename(f.name)],
+			stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	out, err = p.communicate()
+
+	if p.returncode != 0:
+		raise ValueError("parsing PKGBUILD failed")
+	else:
+		for l in out.decode("utf-8", "ignore").splitlines():
+			m = re.match('[\s]*([a-z][a-zA-Z_0-9]*)\+?=', l)
+			if not m:
+				continue
+			varnames.append(m.group(1))
+
+	os.unlink(f.name)
+	return set(varnames)
 
 class package(PkgbuildRule):
 	name = "extravars"
@@ -31,12 +70,11 @@ class package(PkgbuildRule):
 				 'pkgbase', 'pkgver', 'pkgrel', 'pkgdesc', 'groups',
 				 'url', 'install', 'changelog',
 				 'options', 'optdepends']
-		for i in pkginfo.pkgbuild:
-			m = re.match('[\s]*([a-z][^\s=]*[^+])=', i)
-			if m:
-				varname = m.group(1)
-				if varname not in stdvars:
-					if not varname.startswith('_'):
-						self.warnings.append(("extra-var-begins-without-underscore %s", varname))
+		pkgbuild_vars = find_lowercase_global_vars("\n".join(pkginfo.pkgbuild))
+		nonstdvars = pkgbuild_vars - set(stdvars)
+		self.warnings.extend(
+			[("extra-var-begins-without-underscore %s", varname) for
+				varname in nonstdvars]
+			)
 
 # vim: set ts=4 sw=4 noet:
--- Namcap/tests/pkgbuild/test_extravars.py
+++ Namcap/tests/pkgbuild/test_extravars.py
@@ -56,6 +56,44 @@ package() {
   make DESTDIR="${pkgdir}" install
 }
 """
+	# now a more tricky example
+	pkgbuild2 = """
+# Maintainer: Arch Linux <archlinux@example.com>
+# Contributor: Arch Linux <archlinux@example.com>
+
+pkgname=mypackage
+pkgver=1.0
+pkgrel=1
+pkgdesc="A pac
+kage=1 package"
+arch=('i686' 'x86_64')
+url="http://www.example.com/"
+license=('GPL')
+depends=('glibc')
+depends+=('glib2'
+glibc=2.12
+)
+optdepends=('gtk2')
+options=('!libtool')
+source=(ftp://ftp.example.com/pub/mypackage-0.1.tar.gz)
+md5sums=('abcdefabcdef12345678901234567890')
+
+build() {
+  cd "${srcdir}"/${pkgname}-${pkgver}
+  ./configure --prefix=/usr \\
+	--libdir=something
+  _variable=value make; mycustomvar="something"
+  hello=world make something
+  echo 's/a/b/
+        b=c
+        s/c=d/'
+}
+
+package() {
+  cd "${srcdir}"/${pkgname}-${pkgver}
+  make DESTDIR="${pkgdir}" install
+}
+"""
 
 	test_valid = PkgbuildTest.valid_tests
 
@@ -71,5 +109,16 @@ package() {
 			("extra-var-begins-without-underscore %s", "hello")
 		]))
 		self.assertEqual(r.infos, [])
+
+	def test_example2(self):
+		"a tricky PKGBUILD with custom variables without underscore"
+		r = self.run_on_pkg(self.pkgbuild2)
+		self.assertEqual(r.errors, [])
+		self.assertEqual(set(r.warnings), set([
+			("extra-var-begins-without-underscore %s", "mycustomvar"),
+			("extra-var-begins-without-underscore %s", "hello")
+		]))
+		self.assertEqual(r.infos, [])
+
 
 # vim: set ts=4 sw=4 noet:
