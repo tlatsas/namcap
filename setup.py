afd6bbaf919e27f296eb4844800b766a03fd51bb
Rémy Oudompheng <remy@archlinux.org>
2011-04-02 04:31:30 +0200

    Release version 3.0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ DATAFILES = [('/usr/share/man/man1', ['namcap.1']),
 		('/usr/share/doc/namcap',['README','AUTHORS','TODO'])]
 
 setup(name="namcap",
-	version="3.0.5",
+	version="3.0.6",
 
 	description="Pacman package analyzer",
 	author="Arch Dev Team",
